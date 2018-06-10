7b30101b9d5c076ce2513a0e3547271966985106
John Eleen <Jeleen@users.noreply.github.com>
2018-06-09 17:28:21 -0700

    First Partition

--- FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -29,6 +29,34 @@ public class UrlValidatorTest extends TestCase {
    public void testYourFirstPartition()
    {
 	 //You can use this function to implement your First Partition testing	   
+	         
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+      // StringBuilder testBuffer = new StringBuilder();
+   		String url = "www.google.com";
+   		int pass=0; int fail=0;
+   		String [] TestUrlScheme = {"","http://","h3t://","3ht://","https://};
+   			
+   			for(int i=0;i< TestUrlScheme.length;i++){
+   			//check even, valid, scheme elements… should return true
+   					if(urlVal.isValid(TestUrlScheme[i] + url)){
+	   						if(printStatus){
+	   						System.out.print("Expected: true" +"\t");
+	   						System.out.println("result: " +urlVal.isValid(TestUrlScheme[i] + url));
+	   						}
+	   						//boolean	expected=false;
+	   						//assertEquals(TestUrlScheme[i] + url,expected,urlVal.isValid(TestUrlScheme[i] + url));
+   						pass++;}
+   					if(!urlVal.isValid(TestUrlScheme[i] + url)){
+	   						if(printStatus){
+	   						System.out.print("Expected: false" +"\t");
+	   						System.out.println("result: " +urlVal.isValid(TestUrlScheme[i] + url));
+	   						}
+	   						//boolean	expected=false;
+   						fail++;
+   					}
+   			}
+   			System.out.println("Scheme Tests:");
+   			System.out.println("Pass:"+pass+"--Fail:"+fail);
 
    }
    
