5229cdbe3afec8671e983a1e459ac056caf28bfb
Paige <riberap@oregonstate.edu>
2018-06-10 04:01:57 +0000

    manual tests written

--- projects/riberap/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
+++ projects/riberap/FinalProject/URLValidatorInCorrect/test/UrlValidatorTest.java
@@ -7,9 +7,6 @@ import junit.framework.TestCase;
 // Again, it is up to you to use this file or not!
 
 
-
-
-
 public class UrlValidatorTest extends TestCase {
 
 
@@ -17,33 +14,99 @@ public class UrlValidatorTest extends TestCase {
       super(testName);
    }
 
-   
-   
+
+
    public void testManualTest()
    {
-//You can use this function to implement your manual testing	   
-	   
+//You can use this function to implement your manual testing
+
+    UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+    System.out.println("Starting manual testing ...");
+
+    int passTests = 0;
+    int failTests = 0;
+    int totalTests = 0;
+
+    if (urlVal.isValid("http://www.google.com")) {
+        passTests++;
+    } else {
+        failTests++;
+    }
+    totalTests++;
+
+    if (urlVal.isValid("ftp://go.com") {
+        passTests++;
+    } else {
+        failTests++;
+    }
+    totalTests++;
+
+    if (urlVal.isValid("h3t://go.au") {
+        passTests++;
+    } else {
+        failTests++;
+    }
+    totalTests++;
+
+    if (urlVal.isValid("://256.256.256.256") {
+        passTests++;
+    } else {
+        failTests++;
+    }
+    totalTests++;
+
+    if (urlVal.isValid("http:1.2.3") {
+        passTests++;
+    } else {
+        failTests++;
+    }
+    totalTests++;
+
+    if (urlVal.isValid("3ht://go.com") {
+        passTests++;
+    } else {
+        failTests++;
+    }
+    totalTests++;
+
+    if (urlVal.isValid("http://255.255.255.255") {
+        passTests;
+    } else {
+        failTests++;
+    }
+    totalTests++;
+
+    if (urlVal.isValid("go.1aa") {
+        passTests++;
+    } else {
+        failTests++;
+    }
+    totalTests++;
+
+    System.out.println("Total tests run...%d", totalTests);
+    System.out.println("Total tests failed...%d", failTests);
+    System.out.println("Total tests passed...%d", passTests);
    }
-   
-   
+
+
    public void testYourFirstPartition()
    {
-	 //You can use this function to implement your First Partition testing	   
+	 //You can use this function to implement your First Partition testing
 
    }
-   
+
    public void testYourSecondPartition(){
-		 //You can use this function to implement your Second Partition testing	   
+		 //You can use this function to implement your Second Partition testing
 
    }
-   //You need to create more test cases for your Partitions if you need to 
-   
+   //You need to create more test cases for your Partitions if you need to
+
    public void testIsValid()
    {
 	   //You can use this function for programming based testing
 
    }
-   
+
 
 
 }
